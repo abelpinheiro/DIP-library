a8338cf67ddcb9c8469d3763eebc6b1b0cf565e0
Abel <abelpinheiro95@gmail.com>
2022-04-16 23:10:48 -0300

    Add Signals introduction. Add base other introduction base contents

---
+++ 01 - Signals and Systems/Signals.m
@@ -0,0 +1,52 @@
+% 16/04/2022 - @Abel Pinheiro de Figueiredo
+% This script presents some of the most important 
+% and used signals as well operations
+
+% Range value
+t = (-5:0.01:5);
+
+% Unit Step function | Heaviside Step function
+% u(t) = 1 if t >= 0 | 0 if t < 0
+u = inline('(t >= 0)', 't');
+unitstep = t>=0;
+
+
+% Unit Impulse function
+% f(t) = 1 if t = 0 | 0 if t != 0
+f = inline('(t == 0)', 't');
+impulse = t==0;
+
+% Unit Pulse function (combination of unit steps)
+% p(t) = u(t) - u(t - 1)
+p = inline('(t >= 0)&(t < 1)', 't');
+
+% Ramp signal
+ramp = t.*unitstep;
+
+
+%TODO sinusoidal signals? Exponential signals?
+
+% Plot signals
+subplot(2,2,1);
+plot(t, u(t));
+xlabel('t');ylabel('u(t)');
+axis([-2 2 -0.1 1.1]);
+title('Unit Step');
+
+subplot(2,2,2);
+plot(t, f(t));
+xlabel('t');ylabel('f(t)');
+axis([-2 2 -0.1 1.1]);
+title('Unit Impulse');
+
+subplot(2,2,3);
+plot(t, p(t));
+xlabel('t');ylabel('p(t)');
+axis([-2 2 -0.1 1.1]);
+title('Unit Pulse');
+
+subplot(2,2,4);
+plot(t, ramp);
+xlabel('t');ylabel('r(t)');
+axis([-2 2 -0.1 2.5]);
+title('Ramp');
---
+++ 01 - Signals and Systems/signals_operations.m
@@ -0,0 +1,7 @@
+t = (-5:0.01:5);
+
+%escalonamento
+%deslocamento
+%reversão
+
+%% com imagens
